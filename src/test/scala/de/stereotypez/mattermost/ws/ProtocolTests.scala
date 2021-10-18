5dc64d0404304d3921b3fcda3ce392b7340cf622
Moritz Ulrich <moritz@tarn-vedra.de>
2021-10-18 14:32:50 +0200

    Fix tests after merge

--- src/test/scala/de/stereotypez/mattermost/ws/ProtocolTests.scala
+++ src/test/scala/de/stereotypez/mattermost/ws/ProtocolTests.scala
@@ -3,60 +3,39 @@ package de.stereotypez.mattermost.ws
 
 import de.stereotypez.mattermost.ws.events._
 import de.stereotypez.mattermost.ws.Protocol._
-import de.stereotypez.mattermost.ws.Protocol.JsonProtocol._
-
-import org.junit.Test
-import org.junit.Assert._
-
-import de.stereotypez.mattermost.ws.Protocol.{Broadcast, Data, EventMessage, Post, StatusMessage}
 
 import spray.json._
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.matchers.should.Matchers
 
-class ProtocolTests extends AnyFunSuite {
-  test("test-01") {
+class ProtocolTests extends AnyFunSuite with Matchers {
+  test("test01") {
     val txt = """{"omit_users":null,"user_id":"zt3rb4qfi7bm3gfk31c97a3fih","channel_id":"","team_id":""}"""
     txt.parseJson.convertTo[Broadcast]
   }
 
-  test("test-02") {
-    val txt = """{"event": "status_change", "broadcast": {"omit_users":null,"user_id":"zt3rb4qfi7bm3gfk31c97a3fih","channel_id":"","team_id":""}, "seq": 1}"""
-    txt.parseJson.convertTo[EventMessage]
+  
+  test("test02")  {
+    val txt = """{"event": "status_change", "data": {"status":"online","user_id":"4ac3xh974jgmffq8rdowxunjma"}, "broadcast": {"omit_users":null,"user_id":"zt3rb4qfi7bm3gfk31c97a3fih","channel_id":"","team_id":""}, "seq": 1}"""
+    txt.parseJson.convertTo[WebsocketEvent]
   }
 
-  test("test-03") {
-    val txt = """{"event": "status_change", "data": null, "broadcast": {"omit_users":null,"user_id":"zt3rb4qfi7bm3gfk31c97a3fih","channel_id":"","team_id":""}, "seq": 1}"""
-    txt.parseJson.convertTo[EventMessage]
-  }
-
-  test("test-04"){
+  
+  test("test03") {
     val txt = """{"status":"online","user_id":"zt3rb4qfi7bm3gfk31c97a3fih"}"""
-    val data = txt.parseJson.convertTo[Data]
-    data.status should equal(Some("online"))
-    data.user_id should equal(Some("zt3rb4qfi7bm3gfk31c97a3fih"))
-    data.team_id should equal(None)
+    val data = txt.parseJson.convertTo[StatusChange.StatusChangeData]
+    data.status shouldBe "online"
+    data.user_id shouldBe "zt3rb4qfi7bm3gfk31c97a3fih"
   }
 
-  test("test-05"){
-    val txt = """{"id":"r36ktcdsuffq7x67zcstuswr5h","create_at":1601328848778,"update_at":1601328848778,"edit_at":0,"delete_at":0,"is_pinned":false,"user_id":"9nf7d8jrp3r88qkhownk3b1keo","channel_id":"ix8sg36eppbs9yuuqnqottrwme","root_id":"","parent_id":"","original_id":"","message":"Wilhelm Nensa","type":"","props":{"disable_group_highlight":true},"hashtags":"","pending_post_id":"9nf7d8jrp3r88qkhownk3b1keo:1601328851756","reply_count":0,"metadata":{}}"""
-    txt.parseJson.convertTo[Post]
-  }
-
-  test("test-07") {
-
-    val txt = """{"event": "status_change", "data": {"status":"online","user_id":"zt3rb4qfi7bm3gfk31c97a3fih"}, "broadcast": {"omit_users":null,"user_id":"zt3rb4qfi7bm3gfk31c97a3fih","channel_id":"","team_id":""}, "seq": 1}"""
-
-    txt.parseJson.asJsObject match {
-      case ast if ast.fields.contains("event") =>
-        val event = ast.convertTo[EventMessage]
-        event.data.flatMap(_.status) should equal(Some("online"))
-        event.data.flatMap(_.post) should equal(None)
-
-      case ast if ast.fields.contains("status") =>
-        ast.convertTo[StatusMessage]
-      case _ => throw new MatterMostWsProtocolException("Unknown message type:\n" + txt.parseJson.prettyPrint)
+  test("test04") {
+    val txt = """{"event":"posted","data":{"channel_display_name":"@admin","channel_name":"4ac3xh974jgmffq8rdowxunjma__wb7dpg3tsfrimxcu8767ig55sh","channel_type":"D","mentions":"[\"4ac3xh974jgmffq8rdowxunjma\"]","post":"{\"id\":\"xo8gwpzsq78tur5soq47cjtskh\",\"create_at\":1633085267810,\"update_at\":1633085267810,\"edit_at\":0,\"delete_at\":0,\"is_pinned\":false,\"user_id\":\"wb7dpg3tsfrimxcu8767ig55sh\",\"channel_id\":\"3xqxgouhqjnm5k49qohjort7yc\",\"root_id\":\"\",\"parent_id\":\"\",\"original_id\":\"\",\"message\":\"oh there once was a hero named ragnar the red\",\"type\":\"\",\"props\":{\"disable_group_highlight\":true},\"hashtags\":\"\",\"pending_post_id\":\"wb7dpg3tsfrimxcu8767ig55sh:1633085267773\",\"reply_count\":0,\"last_reply_at\":0,\"participants\":null,\"is_following\":false,\"metadata\":{}}","sender_name":"@admin","set_online":true,"team_id":""},"broadcast":{"omit_users":null,"user_id":"","channel_id":"3xqxgouhqjnm5k49qohjort7yc","team_id":""},"seq":3}"""
+    val event = txt.parseJson.convertTo[WebsocketEvent]
+    event.data match {
+      case data: Posted.PostData =>
+        val post = data.post.parseJson.convertTo[Posted.Post]
+        post.id shouldBe "xo8gwpzsq78tur5soq47cjtskh"
+      case _=> assert(false)
     }
   }
-
 }
